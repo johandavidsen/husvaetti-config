4c087f26f62ed29da500a1192648efe71bcf8b39
Jóhan Dam-Davidsen <johan.davidsen@fjakkarin.com>
2021-12-02 16:45:03 -0300

    Updated gitignore

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++
@@ -1,9 +0,0 @@
-
-try {
-  new Function("import('/hacsfiles/frontend/main-8113c4f2.js')")();
-} catch (err) {
-  var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-8113c4f2.js';
-  document.body.appendChild(el);
-}
-  
